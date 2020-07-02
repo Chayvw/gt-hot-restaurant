92d183f7725349a161fb552cf9117d43f86b445a
Chay Williams <Chayvw@gmail.com>
2020-07-02 14:27:46 -0400

    updated objects for js file

--- server.js
+++ server.js
@@ -2,21 +2,36 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = prcess.env.PORT || 3000;
-app.listen(PORT, ()=> {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("APP listening on PORT " + PORT)
 
 });
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"))
 });
-app.get("/", function(req, res){
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"))
-})
+});
+app.get("/table", function (req, res) {
+    res.sendFile(path.join(__dirname, "table.html"))
+});
+
+// app.get
 
 
-// const reservationIfo = [ {
-//     input: ,
-//     name:
-// }]
 
+const reservationInfo = [
+    {
+        type:"input",
+        message: "First and last name",
+        name: "name"
+    }   
+]
+const waitListInfo = [
+    {
+        type:"input",
+        message: "First and last name",
+        name: "name"
+    }   
+]
